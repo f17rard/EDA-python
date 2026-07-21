15784524064fd0b7db2f4ee10f1646efe609b297
Gabriel <gabomendua@gmail.com>
2026-07-21 08:27:06 -0600

    algoritmos voraces completos

--- algoritmos_voraces.py
+++ algoritmos_voraces.py
@@ -1,4 +1,4 @@
-"""Ejercicio del cambio
+"""# Ejercicio del cambio
 denominaciones = [100, 20, 10, 5, 1]
 
 montos = [47, 83, 156, 1, 3]
@@ -12,10 +12,10 @@ for monto in montos:
             num_billetes[i] += 1
             
     print(num_billetes)
+"""
 
-""" 
 
-"""Ejercicio de la mochila"""
+""" # Ejercicio de la mochila
 def ProblemaMochila(Peso: int, valores: list, pesos: list):
     soluciones = [0.0*i for i in range(len(valores))]
     valor_peso = [valores[i]/pesos[i] for i in range(len(valores))]
@@ -67,3 +67,31 @@ if __name__ == "__main__":
         ]
     for c in casos:
         print(c, ProblemaMochila(*c))
+"""
+
+"""Ejercicio de los trabajadores"""
+trabajadores = ["A","B","C","D"]
+tareas = ["T1","T2","T3","T4"]
+costos = [
+    [9, 2, 7, 8],
+    [6, 4, 3, 7],
+    [5, 8, 1, 8],
+    [7, 6, 9, 4],
+]
+
+def mejores_costos(Workers, Tasks, Costs:list):
+    costos_minimos = []
+    tareas_asignadas = []
+    for i in range(len(Workers)):
+        menor_tarea = float("inf")
+        menor_indice = None
+        for j in range(len(Tasks)):
+            if menor_tarea > Costs[i][j] and Tasks[j] not in tareas_asignadas:
+                menor_tarea = Costs[i][j]
+                menor_indice = j
+        tareas_asignadas.append(Tasks[menor_indice])
+        costos_minimos.append((Workers[i],Tasks[menor_indice]))
+        
+    return costos_minimos
+
+print(mejores_costos(trabajadores, tareas, costos))
